d35a97dc2a94d68db441a74b200444632e6c0a0e
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-11-07 15:21:14 +0500

    ✍🏻 Edit regions

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Grid arrangement of regions and elements on the screen
 
-![Grid arrangement of regions and elements on the screen](https://imgur.com/Hxyl4qv.jpg)
+![Grid arrangement of regions and elements on the screen](https://imgur.com/CSNRJ7N.jpg)
--- src/initConfig.ts
+++ src/initConfig.ts
@@ -2,10 +2,10 @@ import { Point, Region } from '@nut-tree/nut-js';
 import { Config, Messanger } from './state/types';
 
 export const initConfig = (screenWidth: number, screenHeight: number, messanger: Messanger): Config => ({
-	fishingPlaceRegion: new Region(0, 0, screenWidth, screenHeight),
-	lmbRegion: new Region(0, 0, screenWidth, screenHeight),
-	successRegion: new Region(0, 0, screenWidth, screenHeight),
-	errorRegion: new Region(0, 0, screenWidth, screenHeight),
+	fishingPlaceRegion: new Region(0, screenHeight / 2, screenWidth / 2, screenHeight / 2),
+	lmbRegion: new Region(screenWidth / 2, screenHeight / 2, screenWidth, screenHeight),
+	successRegion: new Region(0, screenHeight / 2, screenWidth / 2, screenHeight / 2),
+	errorRegion: new Region(0, screenHeight / 2, screenWidth / 2, screenHeight / 2),
 	startMousePosition: new Point(screenWidth / 2, screenHeight / 2),
 	clickingDirection: false,
 	startedInLast10sec: false,
