706b931d92de045d33a2d14d11b118ea08503d38
Yaniddze <facecrack1337@gmail.com>
2021-11-08 00:18:32 +0500

    Небольшие изменения :rocket:

--- src/state/fishing/waitLmd/waitLmdSwitch.ts
+++ src/state/fishing/waitLmd/waitLmdSwitch.ts
@@ -2,6 +2,7 @@ import { waitForImage } from '@utils/waitForImage';
 import { createParam } from '@utils/parameterFactory';
 
 import { clickingState } from '../clicking';
+import { startState } from '../start';
 
 import { FishingSwitch } from '../types';
 
@@ -9,9 +10,12 @@ export const waitLmdSwitch: FishingSwitch = async (config) => {
 	const { lmbRegion } = config;
 	const lmbSearchParam = createParam(lmbRegion, 0.99);
 
-	const lmbIndicator = await waitForImage('lmb.png', 65000, lmbSearchParam);
+	try {
+		const lmbIndicator = await waitForImage('lmb.png', 65000, lmbSearchParam);
+		config.lmbRegion = lmbIndicator;
 
-	config.lmbRegion = lmbIndicator;
-
-	return clickingState;
+		return clickingState;
+	} catch {
+		return startState;
+	}
 };
