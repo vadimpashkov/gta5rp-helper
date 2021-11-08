206fc5d582923933b414fc659b51b50551c58707
Yaniddze <facecrack1337@gmail.com>
2021-11-09 02:45:01 +0500

    Тыкалка мышкой :zap:

--- src/state/fishing/clicking/clickingIteration.ts
+++ src/state/fishing/clicking/clickingIteration.ts
@@ -1,14 +1,16 @@
+import { left, mouse, right } from '@nut-tree/nut-js';
 import { mouseClick } from '@utils/mouseClick';
 
+import { getRandomIntInclusive } from '@utils/getRandomIntInclusive';
 import { FishingIteration } from '../types';
 
 export const clickingIteration: FishingIteration = async (config) => {
 	const { clickingDirection: direction } = config;
-	try {
-		mouseClick(config.startMousePosition.x, config.startMousePosition.y);
-	} catch (e) {
-		console.log(e);
-	}
+	const moveSize = getRandomIntInclusive(100, 200);
+
+	await mouse.move(direction ? left(moveSize) : right(moveSize));
+
+	mouseClick(config.startMousePosition.x, config.startMousePosition.y);
 
 	config.clickingDirection = !direction;
 };
