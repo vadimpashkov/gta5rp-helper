6da799f3a9fa3c07a8fb7228cf45fe9fcf03d19c
Yaniddze <facecrack1337@gmail.com>
2021-11-20 00:14:03 +0500

    Добавил initialState :ambulance:

--- src/store/settings/settingsStore.ts
+++ src/store/settings/settingsStore.ts
@@ -1,6 +1,9 @@
 import { store } from '../innerStore';
 import { Settings } from './types';
 
-export const getSettings = (): Settings => store.get('settings') as Settings;
+export const getSettings = (): Settings =>
+	(store.get('settings') as Settings) || {
+		doubleClick: false,
+	};
 
 export const setSettings = (newSettings: Settings) => store.set('settings', newSettings);
