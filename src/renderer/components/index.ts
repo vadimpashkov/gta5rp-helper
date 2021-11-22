8c1eb4a3c8354a05c6f86b719f181d94430524fc
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-11-22 22:48:33 +0300

    👺 Edit FishingPage

--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -1,8 +1,15 @@
+export * from './ToggleSwitch';
+export * from './InfoPanel';
+
 export * from './MainWrapper';
 export * from './DragPanel';
+
 export * from './CloseButton';
 export * from './ComebackButton';
-export * from './Svg';
+
 export * from './FishingCard';
 export * from './FishingDashboard';
-export * from './ToggleSwitch';
+export * from './FishingInformation';
+export * from './BeforeTheFishPriceUpdate';
+
+export * from './Svg';
