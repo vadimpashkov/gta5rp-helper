45a813da785b64cedfde58153b0f6371d8c59a32
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-11-25 20:59:36 +0300

    😮 Add sort for totalFishStore

--- src/renderer/stores/totalFishStore.ts
+++ src/renderer/stores/totalFishStore.ts
@@ -3,7 +3,7 @@ import { atom } from 'nanostores';
 import { useStore } from '@nanostores/react';
 
 import { receiveEvent, sendEvent } from '../utils';
-import { TotalFish, initTotalFish } from '../../core';
+import { TotalFish, initTotalFish, sortFish, groupFish } from '../../core';
 
 const store = atom<TotalFish>(initTotalFish());
 
@@ -15,5 +15,11 @@ export const useTotalFish = () => {
 	useEffect(() => {
 		sendEvent('giveMeTotalFish');
 	}, []);
-	return useStore(store);
+
+	const fish = useStore(store);
+	const sorted = Object.entries(fish)
+		.sort(([, a], [, b]) => b - a)
+		.reduce((acum, [key, value]) => ({ ...acum, [key]: value }), {});
+
+	return sorted;
 };
