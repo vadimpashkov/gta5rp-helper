718083c89ab4630925be34fa37db4dde342bcc64
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-11-28 02:26:32 +0300

    😮 Add addDot function in extractNumberFromWeight

--- src/utils/extractNumberFromWeight.ts
+++ src/utils/extractNumberFromWeight.ts
@@ -1,31 +1,38 @@
+function addDot(value: string, total: number, offset = 1): number {
+	let valueLength = value.length;
+
+	if (valueLength >= 2) {
+		// 185 из 205 это может быть как 1.85 так и 185 (если багажник)
+		let result = '';
+
+		result += value.substring(0, valueLength - offset);
+		result += '.';
+		result += value.substring(valueLength - offset, valueLength + 1);
+
+		console.log(parseFloat(result));
+
+		return parseFloat(result) > total ? addDot(value, total, ++offset) : Math.ceil(parseFloat(result) * 100) / 100;
+	}
+
+	return Number(value);
+}
+
 export const extractNumbersFromWeight = (weight: string) => {
 	const preparedText = weight
-		.replace(/[^0-9.\/\s]/g, ' ')
+		.replace(/[^0-9.\s]/g, ' ')
 		.replace(/ +/g, ' ')
 		.trim();
-	const match = preparedText.match(/(\d+.?\d?\d?)(\/|\s)(\d+)/);
+	const match = preparedText.match(/(\d+\.?\d?\d?)( )(\d+)/);
 
-	console.log(weight, preparedText);
+	console.log(weight, preparedText, match);
 
 	if (match === null) throw Error('Weight not found');
 
 	const total = Number(match[3]);
 
 	let currentString = match[1];
-	let currentStringLength = currentString.length;
 
-	if (!currentString.includes('.') && currentStringLength >= 2 && Number(currentString) > total) {
-		// 185 из 205 это может быть как 1.85 так и 185 (если багажник)
-		let resultString = '';
-
-		resultString += currentString.substring(0, currentStringLength - 2);
-		resultString += '.';
-		resultString += currentString.substring(currentStringLength - 2, currentStringLength + 1);
-
-		currentString = resultString;
-	}
-
-	const current = Number(currentString);
+	const current = !currentString.includes('.') ? addDot(currentString, total) : Number(currentString);
 
 	console.log({
 		current,
