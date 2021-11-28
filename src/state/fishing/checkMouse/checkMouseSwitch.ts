1ec61b706848e0cbe0b05fc23bd9d69efb02ed9b
Yan <49100328+Yaniddze@users.noreply.github.com>
2021-11-29 00:28:30 +0500

    Таймаут :ambulance:

--- src/state/fishing/checkMouse/checkMouseSwitch.ts
+++ src/state/fishing/checkMouse/checkMouseSwitch.ts
@@ -7,15 +7,21 @@ import { successState } from '../success';
 
 import { FishingConfig, FishingState, FishingSwitch } from '../types';
 
-export const checkMouseSwitch: FishingSwitch = createCancelable<FishingConfig, FishingState>(async (config) => {
-	const { mouseRegion } = config;
-	const lmbSearchParam = createParam(mouseRegion, 0.8);
+export const checkMouseSwitch: FishingSwitch = createCancelable<FishingConfig, FishingState>(
+	(config) =>
+		new Promise((resolve) => {
+			// Изображение ошибки появляется слишком быстро
+			setTimeout(async () => {
+				const { mouseRegion } = config;
+				const lmbSearchParam = createParam(mouseRegion, 0.8);
 
-	try {
-		const region = await waitForImage('mouse.png', 1000, lmbSearchParam);
-		config.mouseRegion = region;
-		return waitLmdState;
-	} catch {
-		return successState;
-	}
-});
+				try {
+					const region = await waitForImage('mouse.png', 1000, lmbSearchParam);
+					config.mouseRegion = region;
+					resolve(waitLmdState);
+				} catch {
+					resolve(successState);
+				}
+			}, 300);
+		}),
+);
