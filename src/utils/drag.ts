0e357411f19145e9b9339e4d51c01953cd3fb1bc
Yan <49100328+Yaniddze@users.noreply.github.com>
2021-12-01 23:50:08 +0500

    Поменял скорость :zap:

--- src/utils/drag.ts
+++ src/utils/drag.ts
@@ -7,7 +7,7 @@ type Coordinate = {
 };
 
 export const drag = async (from: Coordinate, to: Coordinate, spped?: number) => {
-	await gtaProcess.mouse.moveCurveToAsync(from.x, from.y, spped || 4, 30);
+	await gtaProcess.mouse.moveCurveToAsync(from.x, from.y, spped || 7, 30);
 
 	await mouse.pressButton(Button.LEFT);
 
