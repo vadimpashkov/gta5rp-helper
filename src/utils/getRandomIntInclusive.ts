f768641f9ed4ad79e6e43fbe442bd727cf734405
Vadim Pashkov <vadimpashkov.job@gmail.com>
2021-12-12 22:11:31 +0300

    💊 Add export to getRandomNumberInclusive

--- src/utils/getRandomIntInclusive.ts
+++ src/utils/getRandomIntInclusive.ts
@@ -1,4 +1,4 @@
-const getRandomNumberInclusive = (min: number, max: number) => {
+export const getRandomNumberInclusive = (min: number, max: number) => {
 	const random = min - 0.5 + Math.random() * (max - min + 1);
 	return Math.round(random);
 };
